45a1a2ecc6b1a164a5ab8749d4f7dfe74023d87d
Christopher Stock <email@christopherstock.de>
2018-04-09 17:56:18 +0200

    Added OpenGraph php file.

---
+++ _ASSETS/jnlp/openGraph.php
@@ -0,0 +1,68 @@
+
+<html>
+
+    <head>
+
+        <meta charset="UTF-8" />
+
+        <title>LWJGL 3D Shooter Engine</title>
+
+        <link rel="icon"          href="favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+        <meta http-equiv="imagetoolbar" content="no">
+
+        <meta name="dc.title" content="LWJGL 3D Shooter Engine">
+        <meta name="dc.description" content="LWJGL 3D Shooter Engine">
+        <meta name="dc.identifier" scheme="url" content="http://shooter.jenetic.de">
+        <meta name="dc.subject" content="LWJGL 3D Shooter Engine">
+        <meta name="dc.creator.personalname" content="LWJGL 3D Shooter Engine">
+        <meta name="dc.creator.personalname.address" scheme="e-mail" content="email@christopherstock.de">
+        <meta name="dc.publisher.corporatename" content="LWJGL 3D Shooter Engine">
+        <meta name="dc.publisher.corporatename.address" scheme="e-mail" content="email@christopherstock.de">
+        <meta name="dc.date" scheme="iso8601" content="2018-04-09">
+        <meta name="dc.rights" scheme="urn" content="LWJGL 3D Shooter Engine">
+
+        <meta name="author" content="LWJGL 3D Shooter Engine">
+        <meta name="publisher" content="LWJGL 3D Shooter Engine">
+        <meta name="copyright" content="LWJGL 3D Shooter Engine">
+        <meta name="keywords" content="LWJGL 3D Shooter Engine">
+        <meta name="description" content="LWJGL 3D Shooter Engine">
+        <meta name="expires" content="never">
+        <meta name="page-type" content="LWJGL 3D Shooter Engine">
+        <meta name="audience" content="all">
+        <meta name="revisit-after" content="4 days">
+        <meta name="robots" content="index follow">
+
+        <meta property="og:image" content="http://shooter.jenetic.de/FacebookBadge.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1196" />
+        <meta property="og:image:height" content="628" />
+        <meta property="og:title" content="LWJGL 3D Shooter Engine" />
+        <meta property="og:url" content="http://shooter.jenetic.de/" />
+        <meta property="og:site_name" content="http://shooter.jenetic.de/" />
+        <meta property="og:description" content="A First Person Shooter in pure Java and OpenGL using the Java Leightweight Game Library (LWJGL) and the Maven build tool." />
+
+    </head>
+
+    <body>
+
+        <table border="0px" cellspacing="0px" cellpadding="0px" align="center" width="100%" style="width: 100%; text-align: center;">
+            <tr>
+                <td>
+
+                    <h1 style="font-family: arial; font-size: 25px;">
+                        Click the duke to launch the 3D Shooter Engine
+                    </h1>
+
+                    <a href="Shooter.jnlp">
+                        <img src="java_rocks.png" border="0px">
+                    </a>
+
+                </td>
+            </tr>
+        </table>
+
+    </body>
+
+</html>
